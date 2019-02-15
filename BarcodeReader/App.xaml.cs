dc174de250d008243b9c3ab02dac4f0107a6903a
DDoSolitary <DDoSolitary@gmail.com>
2019-02-15 13:00:37 +0800

    Remove subscription to DispatcherUnhandledException
    Already handled by AppDomain.UnhandledException.

--- BarcodeReader/App.xaml.cs
+++ BarcodeReader/App.xaml.cs
@@ -7,17 +7,10 @@ namespace BarcodeReader {
 			MessageBox.Show(content, "Error - BarcodeReader", MessageBoxButton.OK, MessageBoxImage.Error);
 		}
 
-		private static void ShowUnexpectedError(object e) {
-			ShowError($"Unexpected error:{Environment.NewLine}{e}");
-		}
-
 		private void Application_Startup(object sender, StartupEventArgs e) {
-			AppDomain.CurrentDomain.UnhandledException += (s, args)
-				=> ShowUnexpectedError(args.ExceptionObject);
-		}
-
-		private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e) {
-			ShowUnexpectedError(e.Exception);
+			AppDomain.CurrentDomain.UnhandledException += (s, args) => {
+				ShowError($"Unexpected error:{Environment.NewLine}{args.ExceptionObject}");
+			};
 		}
 	}
 }
